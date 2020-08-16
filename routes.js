3e9b627954452a629342e786f2bbe1973d003642
Shrena Patel <shrena.p@hotmail.co.uk>
2020-08-16 16:06:51 +1200

    get edit section to link from routes.js

--- routes.js
+++ routes.js
@@ -1,22 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const data = require('./data.json')
-const fs = require('fs')
+// const fs = require('fs')
 
-
-
-router.get('/', (request, response) => {
+router.get('/fruits', (request, response) => {
     response.render('fruits/home', data)
 })
 
-router.get('/:id', (request, response) => {
+router.get('/fruits/:id', (request, response) => {
     let id = request.params.id
 
     let found = data.fruits.find((element) => {
         return element.id == id
     })
     
-    response.render('fruits/fruit', data)
+    response.render('fruits/fruit', found)
 })
+
+router.get('/fruits/edit/:id', (request, response) => {
+    let id = request.params.id
+
+    let found = data.fruits.find((element) => {
+        return element.id == id
+    })
+    
+    response.render('fruits/edit', found)
+
+})
+
+router.post('/fruits/edit/:id', (request, response) => {
+    let id = request.params.id
+
+    let found = data.fruits.find((element) => {
+        return element.id == id
+    })
+
+    found.name = request.body.name
+    found.family = request.body.family
+    found.fact = request.body.fact
+
+    response.redirect('/fruits/edit' + id)
+})
+
+
 
 module.exports = router
